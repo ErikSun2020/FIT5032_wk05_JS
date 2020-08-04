f224013e92f3cac7ead48e65c92c9089abe64645
bsun0009 <bsun0009@student.monash.edu>
2020-08-04 13:40:25 +0800

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_wk05_JS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_wk05_JS.MvcApplication" Language="C#" %>
